6f1219662610f981a674a8a2e1f512039df7529e
shinsuke.okumura <shinryo.1707@gmail.com>
2019-02-12 16:14:44 +0000

    add status column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+	before_action :set_task, only: [:show, :edit, :update, :destroy]
+
 	def index
 		# Taskモデルを用いて、DBの全データを抽出
 		@tasks = Task.all
@@ -24,11 +26,13 @@ class TasksController < ApplicationController
 	end
 	
 	def show
-		@task = Task.find(params[:id])
+		# @task = Task.find(params[:id])
+		# set_task
 	end
 	
 	def update
-		@task = Task.find(params[:id])
+		# @task = Task.find(params[:id])
+		# set_task
 		if @task.update(task_params)
 			flash[:success] = 'Task は正常に更新されました'
 			redirect_to @task
@@ -39,7 +43,8 @@ class TasksController < ApplicationController
 	end
 	
 	def destroy
-		@task = Task.find(params[:id])
+		# @task = Task.find(params[:id])
+		# set_task
 		@task.destroy
 		
 		flash[:success] = 'Task は正常に削除されました'
@@ -47,10 +52,15 @@ class TasksController < ApplicationController
 	end
 	
 	def edit
-		@task = Task.find(params[:id])
+		# @task = Task.find(params[:id])
+		# set_task
 	end
 
 	private
+	
+	def set_task
+		@task = Task.find(params[:id])
+	end
 	
 	# Strong Parameter
 	def task_params
